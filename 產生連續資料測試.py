4a83067bb3c18af0da2925f84e4c93d35845d56c
appletime81 <itrimsl8696@gmail.com>
2021-08-19 18:16:49 +0800

    Update

--- 產生連續資料測試.py
+++ 產生連續資料測試.py
@@ -1,18 +1,51 @@
-# from tensorflow.keras.preprocessing.sequence import TimeseriesGenerator
+import pandas as pd
 import numpy as np
-
-# define dataset
-series = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17])
+from sklearn.preprocessing import MinMaxScaler
 
 
 def generate_data(series):
     x = []
     y = []
     for i in range(len(series)):
-        if i > 7 and len(series[i: i + 8]) == 8:
-            x.append(series[i - 8:i])
-            y.append(series[i: i + 8])
-    print(x)
-    print(y)
+        if i > 7 and len(series[i: i + 8, :]) == 8:
+            x.append(series[i - 8:i, :])
+            y.append(series[i: i + 8, :])
+    return x, y
 
-generate_data(series)
+
+def min_max_scale(df):
+    scaler = MinMaxScaler()
+    new_df = scaler.fit_transform(df)
+    return new_df, scaler
+
+
+if __name__ == '__main__':
+    df = pd.read_csv('2017_2020.csv')
+    col_name = ['MB_TEMP', 'WD_HR', 'WIND_DIREC', 'WIND_SPEED', 'RINFLL', 'PM2.5']
+    data = df[col_name]
+    data_after_scale, scaler = min_max_scale(data)
+
+    print(f"data_after_scale shape : {data_after_scale.shape}")
+
+    x, y = generate_data(data_after_scale)
+    x = np.array(x)
+    # print(x.shape)
+    x = x.reshape(-1, 1, 6)
+    print(x.shape)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # 儲存標準化後的資料
+    # new_df = pd.DataFrame(data_after_scale, columns=col_name)
+    # new_df.to_csv('test.csv', index=False)
